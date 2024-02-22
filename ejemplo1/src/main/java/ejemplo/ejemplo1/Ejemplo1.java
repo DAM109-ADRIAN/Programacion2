5c7803e777c095c92be863a7a10c825de31bcc0d
DAM109-ADRIAN <adrian.dediegofernandez@iesmiguelherrero.com>
2024-02-22 19:20:59 +0100

    hola Adrian

---
+++ ejemplo1/src/main/java/ejemplo/ejemplo1/Ejemplo1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ejemplo.ejemplo1;
+
+/**
+ *
+ * @author adry1
+ */
+public class Ejemplo1 {
+
+    public static void main(String[] args) {
+        System.out.println("hola Adrian!");
+    }
+}
