26a436ab99eb2a0e9cf9df1c2308897357fc5431
DAM109-ADRIAN <119076009+DAM109-ADRIAN@users.noreply.github.com>
2024-02-22 19:53:31 +0100

    no se hacer el mergre

--- ejemplo1/src/main/java/ejemplo/ejemplo1/Ejemplo1.java
+++ ejemplo1/src/main/java/ejemplo/ejemplo1/Ejemplo1.java
@@ -12,5 +12,6 @@ public class Ejemplo1 {
 
     public static void main(String[] args) {
         System.out.println("Adrian de Diego Fernandez!");
+        System.out.println("No se hacer el merge");
     }
 }
